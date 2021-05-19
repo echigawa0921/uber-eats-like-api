b5aa3930ba42dda49e94eea19367cea3805a926c
yuki Echigawa <mh.uc.0921@gmail.com>
2021-05-20 00:40:39 +0900

    React,fakeload

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,9 @@
 import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
+// components
+import Skeleton from '@material-ui/lab/Skeleton';
 
 // apis
 import { fetchRestaurants } from '../apis/restaurants';
@@ -7,13 +11,17 @@ import { fetchRestaurants } from '../apis/restaurants';
 // reducers
 import {
   initialState,
-  restaurantsActionTyps,
+  restaurantsActionTypes,
   restaurantsReducer,
 } from '../reducers/restaurants';
+
+// constants
+import { REQUEST_STATE } from '../constants';
 
 // images
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
+import RestaurantImage from '../images/restaurant-image.jpg';
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -33,15 +41,41 @@ const MainCover = styled.img`
   height: 600px;
 `;
 
+const RestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+
+const RestaurantsContentWrapper = styled.div`
+  width: 450px;
+  height: 300px;
+  padding: 48px;
+`;
+
+const RestaurantsImageNode = styled.img`
+  width: 100%;
+`;
+
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
+
 export const Restaurants = () => {
   const [state, dispatch] = useReducer(restaurantsReducer, initialState);
 
   useEffect(() => {
-    dispatch({ type: restaurantsActionTyps.FETCHING });
+    dispatch({ type: restaurantsActionTypes.FETCHING });
     fetchRestaurants()
       .then((data) =>
         dispatch({
-          type: restaurantsActionTyps.FETCH_SUCCESS,
+          type: restaurantsActionTypes.FETCH_SUCCESS,
           payload: {
             restaurants: data.restaurants
           }
@@ -57,13 +91,27 @@ export const Restaurants = () => {
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
-      {
-        state.restaurantsList.map(restaurant =>
-          <div>
-            {restaurant.name}
-          </div>
-        )
-      }
+
+      <RestaurantsContentsList>
+        {
+          state.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+            </Fragment>
+            :
+            state.restaurantsList.map((item, index) =>
+              <Link to={`/restaurants/${item.id}/foods`} key={index} style={{ textDecoration: 'none' }}>
+                <RestaurantsContentWrapper>
+                  <RestaurantsImageNode src={RestaurantImage} />
+                  <MainText>{item.name}</MainText>
+                  <SubText>{`配送料：${item.fee}円 ${item.time_required}分`}</SubText>
+                </RestaurantsContentWrapper>
+              </Link>
+            )
+        }
+      </RestaurantsContentsList>
     </Fragment>
   )
 }
